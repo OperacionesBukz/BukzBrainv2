1be06a38c7f745dbde2431dd0697cb4c30c53fb3
OperacionesBukz <operaciones@bukz.co>
2026-02-28 08:26:48 -0500

    deploy: improvements v2

---
+++ assets/textarea-FeWkwKSv.js
@@ -0,0 +1,6 @@
+import{v as o,r as t,j as s,d as i}from"./index-zZ0VZZ4p.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=o("Calendar",[["path",{d:"M8 2v4",key:"1cmpym"}],["path",{d:"M16 2v4",key:"4m81vk"}],["rect",{width:"18",height:"18",x:"3",y:"4",rx:"2",key:"1hopcy"}],["path",{d:"M3 10h18",key:"8toen8"}]]),d=t.forwardRef(({className:e,...r},a)=>s.jsx("textarea",{className:i("flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm transition-all duration-200 ease-out placeholder:text-muted-foreground hover:border-ring/50 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring/40 focus-visible:border-ring disabled:cursor-not-allowed disabled:opacity-50",e),ref:a,...r}));d.displayName="Textarea";export{l as C,d as T};
