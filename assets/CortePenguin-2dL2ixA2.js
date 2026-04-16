323941c71ece4681b4c53fc39b88f8324596f2a6
OperacionesBukz <operaciones@bukz.co>
2026-04-16 12:00:39 -0500

    Updates

--- assets/CortePenguin-2dL2ixA2.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Dga1ektM.js";const r=()=>e.jsx("div",{className:"py-8 text-center text-muted-foreground",children:"Próximamente"});export{r as default};
---
+++ assets/CortePenguin-CtZNS3C9.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BB80mM9k.js";const r=()=>e.jsx("div",{className:"py-8 text-center text-muted-foreground",children:"Próximamente"});export{r as default};
